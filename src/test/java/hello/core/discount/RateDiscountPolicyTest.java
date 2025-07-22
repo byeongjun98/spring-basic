e1aec02ab639961252f674bc7d5ad38779c1e2d0
ParkByeongJun <degea98@gmail.com>
2025-07-22 11:09:13 +0900

    [Feat] 17. 새로운 할인 정책 개발

---
+++ src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -0,0 +1,39 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class RateDiscountPolicyTest {
+
+    RateDiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    @Test
+    @DisplayName("Vip는 10% 할인이 적용되어야 한다")
+    void vip_o() {
+        //given
+        Member member = new Member(1L, "memberVip", Grade.VIP);
+        //when
+        discountPolicy.discount(member, 10000);
+
+        // import static org.assertj.core.api.Assertions.* 를 통해 스태틱으로 사용하면 Assertions. 생략 가능
+        assertThat(discountPolicy.discount(member, 10000)).isEqualTo(1000);
+        //then
+    }
+
+    @Test
+    @DisplayName("VIP가 아니면 할인이 적용되지 않아야 한다")
+    void vip_x() {
+        //given
+        Member member = new Member(1L, "memberVip", Grade.BASIC);
+        //when
+        discountPolicy.discount(member, 10000);
+
+        assertThat(discountPolicy.discount(member, 10000)).isEqualTo(1000);
+        //then
+    }
+
+}
